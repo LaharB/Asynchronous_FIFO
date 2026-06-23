86163058b53b7828b842e8246ccc46d5ecddb038
LaharB <laharbaruah96@gmail.com>
2026-06-24 01:12:11 +0530

    Codes files updated

--- rtl/async_FIFO.v
+++ rtl/async_FIFO.v
@@ -3,7 +3,7 @@
 `include "rptr_handler.v"
 `include "fifo_mem.v"
 
-module async_FIFO #(parameter DEPTH = 8, DATA_WIDTH = 8, PTR_WIDTH = 3)(
+module async_FIFO #(parameter DEPTH = 8, DATA_WIDTH = 8)(
     //input data
     input [DATA_WIDTH-1:0] data_in,
     //from write domain 
@@ -16,10 +16,17 @@ module async_FIFO #(parameter DEPTH = 8, DATA_WIDTH = 8, PTR_WIDTH = 3)(
     output reg [DATA_WIDTH-1:0] data_out,
     //output from FIFO
     output reg full,  //coming from write domain
-    output reg empty, //coming from read domain 
+    output reg empty //coming from read domain 
 );
+    //FIFO DEPTH calc
+    parameter PTR_WIDTH = $clog2(DEPTH);
 
-    parameter 
+    //wires 
+    wire [PTR_WIDTH:0] g_wptr_sync, g_rptr_sync;
+    wire [PTR_WIDTH:0] b_wptr, b_rptr;
+    wire [PTR_WIDTH:0] g_wptr, g_rptr;
+
+
 
 
 endmodule

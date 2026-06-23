bb15d27d9f68524b052aa1a18533b614b322a95a
LaharB <laharbaruah96@gmail.com>
2026-06-24 00:27:11 +0530

    Codes files updated

--- rtl/rptr_handler.v
+++ rtl/rptr_handler.v
@@ -2,11 +2,49 @@ module rptr_handler #(parameter PTR_WIDTH = 3)(
     input rclk, rrst_n, r_en,
     input [PTR_WIDTH:0] g_wptr_sync, //coming from write domain through Synchr
     output reg [PTR_WIDTH:0] b_rptr, 
-    output reg [PTR_WIDTH:0] g_prtr //going to write domain through Synchr
+    output reg [PTR_WIDTH:0] g_rptr, //going to write domain through Synchr
+    output reg empty
 );
 
-    wire [PTR_WIDTH:0] b_rptr_next; //next value of b_rptr_next
-    wire [PTR_WIDTH:0] g_rptr_next; //
+    wire [PTR_WIDTH:0] b_rptr_next; //to store next value of b_rptr
+    wire [PTR_WIDTH:0] g_rptr_next; //to store next value of g_rptr
+
+    wire rempty; //to store status of 
+
+    //bin2gray 
+    assign b_rptr_next = b_rptr + (r_en && !empty);
+    assign g_rptr_next = (b_rptr_next>>1) ^ b_rptr_next;
+
+    //empty flag status computation 
+    assign rempty = (g_wptr_sync == g_rptr_next);
+
+    //always block to update bin and gray read ptr 
+    always@(posedge rclk or negedge rrst_n) //async read reset 
+        begin
+            if(!rrst_n)
+                begin
+                    b_rptr <= 0; //default value
+                    g_rptr <= 0; 
+                end 
+            else
+                begin
+                    b_rptr <= b_rptr_next;
+                    g_rptr <= g_rptr_next;
+                end   
+        end
+    
+    //always block for updating empty flag status
+    always@(posedge rclk or negedge rrst_n)
+        begin
+            if(!rrst_n)
+                begin
+                    empty <= 0; //default value 
+                end 
+            else 
+                begin
+                    empty <= rempty;
+                end
+        end
 
 
 
--- rtl/wptr_handler.v
+++ rtl/wptr_handler.v
@@ -3,7 +3,7 @@ module wptr_handler #(parameter PTR_WIDTH = 3)(
     input [PTR_WIDTH:0] g_rptr_sync, //coming from read domain thorugh synchr
     output reg [PTR_WIDTH:0] b_wptr, //going to FIFO mem 
     output reg [PTR_WIDTH:0] g_wptr, //going to read_domain through synchr
-    output reg full //full flag 
+    output reg full //full flag status going to FIFO mem
 );
 
     wire [PTR_WIDTH:0] b_wptr_next; //var to store next value of b_wptr
@@ -12,11 +12,14 @@ module wptr_handler #(parameter PTR_WIDTH = 3)(
     wire wfull;
 
     //bin2gray coversion
-    assign b_wptr_next = b_wptr + (w_en + !full);
+    assign b_wptr_next = b_wptr + (w_en && !full);
     assign g_wptr_next = (b_wptr_next>>1)^b_wptr_next;
 
-    //always block for incrementing bin and gray write ptr 
-    always@(posedge wclk or negedge wrst_n) //async w_rst 
+    //checking wrap around condition for full flag 
+    assign wfull = (g_wptr_next == {g_rptr_sync[PTR_WIDTH:PTR_WIDTH-1], g_rptr_sync[PTR_WIDTH-2:0]}); //2 MSBs and rest 2 bits
+
+    //always block to update bin and gray write ptr
+    always@(posedge wclk or negedge wrst_n) //async write reset 
         begin
             if(!wrst_n) 
                 begin
@@ -30,7 +33,7 @@ module wptr_handler #(parameter PTR_WIDTH = 3)(
                 end
         end
 
-    //always block for computing full flag status  
+    //always block for updating full flag status  
     always@(posedge wclk or negedge wrst_n)
         begin
             if(!wrst_n)
@@ -42,9 +45,5 @@ module wptr_handler #(parameter PTR_WIDTH = 3)(
                     full <= wfull;
                 end
         end
-
-    //computation of wrap around condition
-    assign wfull = (g_wptr_next == {g_rptr_sync[PTR_WIDTH:PTR_WIDTH-1], g_rptr_sync[PTR_WIDTH-2:0]}); //2 MSBs and rest 2 bits
-
 
 endmodule 

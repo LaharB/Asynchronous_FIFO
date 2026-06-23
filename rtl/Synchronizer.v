bb846cbc4abe6e8f8b7bd4a038394e19ebdf7271
LaharB <laharbaruah96@gmail.com>
2026-06-23 21:48:15 +0530

    Codes files updated

---
+++ rtl/Synchronizer.v
@@ -0,0 +1,22 @@
+module synchronizer #(parameter WIDTH = 3) (
+    input clk,
+    input rst_n,
+    input [WIDTH:0] data_in,
+    output reg [WIDTH:0] data_out
+);
+
+    reg [WIDTH:0] data_ms; //2nd FF of the syncrhronizer
+
+    always@(posedge clk)
+        begin
+            if(!rst_n) begin
+                data_ms  <= 0;
+                data_out <= 0;
+            end 
+            else begin
+                data_ms <= data_in;
+                data_out <= data_ms;
+            end  
+        end
+        
+endmodule 
